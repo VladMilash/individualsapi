0f546fb3e4d3a1d2b6da413dcb568e2a48df65b4
Vlad_Milas <12milash12@gmail.com>
2024-09-29 12:06:53 +0300

    Added securityWebFilterChain

---
+++ src/main/java/com/mvo/individualsapi/config/SecurityConfiguration.java
@@ -0,0 +1,41 @@
+package com.mvo.individualsapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtGrantedAuthoritiesConverterAdapter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import java.util.stream.Stream;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return  http
+                .authorizeExchange(customizer -> customizer
+                        .pathMatchers("/v1/registration").permitAll()
+                        .anyExchange().authenticated())
+                .oauth2ResourceServer(customizer-> customizer.jwt(jwt-> {
+                    ReactiveJwtAuthenticationConverter customJwtAuthenticationConverter = new ReactiveJwtAuthenticationConverter();
+                    customJwtAuthenticationConverter.setPrincipalClaimName("preferred_username");
+                    JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+
+                    JwtGrantedAuthoritiesConverter customJwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+                    customJwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("groups");
+                    customJwtGrantedAuthoritiesConverter.setAuthorityPrefix("");
+
+                    customJwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(
+                            new ReactiveJwtGrantedAuthoritiesConverterAdapter(token ->
+                                    Stream.concat(jwtGrantedAuthoritiesConverter.convert(token).stream(),
+                                                    customJwtGrantedAuthoritiesConverter.convert(token).stream())
+                                            .toList()));
+                    jwt.jwtAuthenticationConverter(customJwtAuthenticationConverter);
+                }))
+                .build();
+    }
+}
