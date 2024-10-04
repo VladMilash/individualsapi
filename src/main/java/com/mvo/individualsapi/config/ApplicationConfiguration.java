24587c7559b4952af3b9676587a7fdbe259423ac
Vlad_Milas <12milash12@gmail.com>
2024-10-04 11:40:34 +0300

    Refactored UserinfoServiceImpl

--- src/main/java/com/mvo/individualsapi/config/ApplicationConfiguration.java
+++ src/main/java/com/mvo/individualsapi/config/ApplicationConfiguration.java
@@ -1,8 +1,5 @@
 package com.mvo.individualsapi.config;
 
-import org.jboss.resteasy.client.jaxrs.internal.ResteasyClientBuilderImpl;
-import org.keycloak.admin.client.Keycloak;
-import org.keycloak.admin.client.KeycloakBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/mvo/individualsapi/service/impl/UserinfoServiceImpl.java
+++ src/main/java/com/mvo/individualsapi/service/impl/UserinfoServiceImpl.java
@@ -4,6 +4,7 @@ import com.mvo.individualsapi.dto.UserinfoResponseDTO;
 import com.mvo.individualsapi.service.UserinfoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -12,11 +13,16 @@ import reactor.core.publisher.Mono;
 @Service
 @Slf4j
 public class UserinfoServiceImpl implements UserinfoService {
+
+    @Value("${spring.security.oauth2.client.provider.keycloak.issuer-uri}")
+    private String keycloakIssuerUri;
+
     private final WebClient webClient;
+
     @Override
     public Mono<UserinfoResponseDTO> getUserinfo(String token) {
         return webClient.get()
-                .uri("http://localhost:8180/realms/individualsAPI/protocol/openid-connect/userinfo")
+                .uri(keycloakIssuerUri + "/protocol/openid-connect/userinfo")
                 .headers(headers -> headers.setBearerAuth(token))
                 .retrieve()
                 .bodyToMono(UserinfoResponseDTO.class)
